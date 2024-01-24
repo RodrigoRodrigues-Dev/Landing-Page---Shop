ce14b4639b2618c9a123bbb78dc47d94fceb7468
Rodrigo Rodrigues <rs9844660@gmail.com>
2024-01-24 15:21:37 -0300

    criando a contagem regressiva

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -579,6 +579,26 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
+document.addEventListener("DOMContentLoaded", ()=>{
+    const promotionDuration = new Date("jan 25, 2024 00:00:00");
+    const timeStampPromotion = promotionDuration.getTime();
+    const countsHours = setInterval(()=>{
+        const now = new Date();
+        const timeStampNow = now.getTime();
+        const eventTime = timeStampPromotion - timeStampNow;
+        const hoursUntilTheEvent = Math.floor(eventTime % 86400000 / 3600000);
+        const minutesUntilTheEvent = Math.floor(eventTime % 3600000 / 60000);
+        const secondsUntilTheEvent = Math.floor(eventTime % 60000 / 1000);
+        document.querySelector("#hoursEvents").innerHTML = hoursUntilTheEvent;
+        document.querySelector("#minutesEvents").innerHTML = minutesUntilTheEvent;
+        document.querySelector("#secondsEvents").innerHTML = secondsUntilTheEvent;
+        if (eventTime < 0) {
+            clearInterval(countsHours);
+            document.querySelector(".hero__list").classList.remove("hero__list--is-active");
+            document.querySelector(".soldOff").classList.add("soldOff--is-active");
+        }
+    }, 1000);
+});
 
 },{}]},["eWdrD","3cYfC"], "3cYfC", "parcelRequirea47a")
 
---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const promotionDuration = new Date("jan 25, 2024 00:00:00");
+    const timeStampPromotion = promotionDuration.getTime();
+
+    const countsHours = setInterval(() => {
+        const now = new Date();
+        const timeStampNow = now.getTime();
+
+        const eventTime = timeStampPromotion - timeStampNow;
+
+        const hoursUntilTheEvent = Math.floor((eventTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutesUntilTheEvent = Math.floor((eventTime % (1000 * 60 * 60)) / (1000 * 60));
+        const secondsUntilTheEvent = Math.floor((eventTime % (1000 * 60)) / 1000);
+
+        document.querySelector('#hoursEvents').innerHTML = hoursUntilTheEvent;
+        document.querySelector('#minutesEvents').innerHTML = minutesUntilTheEvent;
+        document.querySelector('#secondsEvents').innerHTML = secondsUntilTheEvent;
+
+        if(eventTime < 0) {
+            clearInterval(countsHours);
+
+            document.querySelector('.hero__list').classList.remove('hero__list--is-active')
+            document.querySelector('.soldOff').classList.add('soldOff--is-active')
+        }
+
+
+    }, 1000);
+});
